9bb69e974c44a69ae81ee793e2943f39d6d00efa
Ayman El Didi <ayman@eldidi.org>
2023-07-08 00:23:16 -0600

    ...

--- server/main.go
+++ server/main.go
@@ -39,7 +39,7 @@ func main() {
 func handler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case http.MethodGet:
-		fmt.Fprintf(w, "%v", Data)
+		fmt.Fprintf(w, "<!DOCTYPE html><body><pre>%v</pre></body>", Data)
 	case http.MethodPost:
 		buf, _ := io.ReadAll(r.Body)
 		err := json.Unmarshal(buf, &Data)
